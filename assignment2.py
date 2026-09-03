6b3ab234e95ba4d96036ca7e4dfb49b64919e466
Shritej <shritejshirude9@gmail.com>
2026-09-03 18:07:44 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,12 @@
+# Largest Of Three Numbers
+a = 10
+b = 20
+c = 30
+
+if a >= b and a >= c:
+    print("Largest number is:", a)
+elif b >= a and b >= c:
+    print("Largest number is:", b)
+else:
+    print("Largest number is:", c)
+
