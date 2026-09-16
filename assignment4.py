3140dd13d8f3b7795eff80ab833e3079fc4711df
Shritej Shirude <shritejshirude9@gmail.com>
2026-09-16 17:05:32 +0530

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,11 @@
+#Matrix Addition 
+import numpy as np
+mat1 = np.array([[1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9]])
+
+mat2 = np.array([[9, 8, 7],
+                  [6, 5, 4],
+                  [3, 2, 1]])
+
+print(mat1+mat2)
