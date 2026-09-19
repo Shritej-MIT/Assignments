84219c2cc595699b2aac187a95ce105c3cac6092
Shritej Shirude <shritejshirude9@gmail.com>
2026-09-19 18:37:15 +0530

    Refactor triangle check function to is_right_triangle

--- assignment3.py
+++ assignment3.py
@@ -1,7 +1,25 @@
 # PYTHON PROGRAM TO CHECK WHETHER THE TRIANGLE IS RIGHT-ANGLED TRIANGLE OR NOT
 
-def check_triangle(a, b, c):
-    if a*a + b*b == c*c:
-        return True
+def is_right_triangle(a, b, c):
+
+    # Find the largest side
+    if a >= b and a >= c:
+        return b * b + c * c == a * a
+
+    elif b >= a and b >= c:
+        return a * a + c * c == b * b
+
     else:
-        return False
+        return a * a + b * b == c * c
+
+
+# Accept three sides
+a = float(input("Enter first side: "))
+b = float(input("Enter second side: "))
+c = float(input("Enter third side: "))
+
+# Function calling
+if is_right_triangle(a, b, c):
+    print("The triangle is a right-angled triangle.")
+else:
+    print("The triangle is not a right-angled triangle.")
